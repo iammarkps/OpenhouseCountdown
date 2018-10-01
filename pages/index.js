f9ed9e113d91de29f824817a0675ceff043f3d53
Possawat Suksai <markkung08@gmail.com>
2018-10-01 18:17:00 +0700

    Finished Project

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Layout from '../components/layout'
+import Countdown from 'react-countdown-now'
+
+export default () => (
+    <Layout>
+        <Countdown date={'Thu, 10 Jan 2019 08:00:00'} />
+    </Layout>
+)
