aab37a719906327de24308356beead489e4835a7
Possawat Suksai <markkung08@gmail.com>
2018-10-03 18:51:39 +0700

    Add prettier

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from '../components/layout'
 import Countdown from 'react-countdown-now'
 
 export default () => (
-    <Layout>
-        <Countdown date={'Thu, 10 Jan 2019 08:00:00'} />
-    </Layout>
+  <Layout>
+    <Countdown date={'Thu, 10 Jan 2019 08:00:00'} />
+  </Layout>
 )
